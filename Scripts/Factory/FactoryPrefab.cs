412b0cd4754ff79b530b6e2e118be78d9143d52c
Dongpn <pndong1986@gmail.com>
2021-07-06 06:52:12 +0700

    Add Factory

---
+++ Scripts/Factory/FactoryPrefab.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using UnityEngine;
+
+namespace Dongpn.Factory
+{
+    public abstract class FactoryPrefab<T> : MonoBehaviour where T : FactoryPrefab<T>
+    {
+        public static T GetInstance()
+        {
+            PrefabsFactory att = (PrefabsFactory)Attribute.GetCustomAttribute(typeof(T), typeof(PrefabsFactory));
+
+            if (att == null || string.IsNullOrEmpty(att.PrimaryKey))
+            {
+                return new GameObject(typeof(T).Name).AddComponent<T>();
+            }
+            else
+            {
+                var prefabs = Resources.Load<GameObject>(att.PrimaryKey);
+                GameObject go = Instantiate(prefabs);
+                T rt = go.GetComponent<T>();
+
+                return rt == null ? go.AddComponent<T>() : rt;
+            }
+        }
+    }
+}
+
